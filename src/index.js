5f590952980f96cd0265b07dbe34e85a494daff2
Toby Williams <Toby.Williams@waitrose.co.uk>
2018-10-31 10:55:05 +0000

    index includes the required js files in browser iframe

---
+++ src/index.js
@@ -0,0 +1,45 @@
+/* eslint-disable func-names */
+const kmhtml = function(files, baseReporterDecorator) {
+  // things that can be passed to the reporter:'baseReporterDecorator', 'formatError', 'config'
+  baseReporterDecorator(this);
+
+  console.log(files);
+
+  // this.onRunStart = function(browsers) {
+  //   this.write('Begin Browsers!');
+  //   return browsers;
+  // };
+};
+
+kmhtml.$inject = ["config.files", "baseReporterDecorator"];
+
+module.exports = {
+  "reporter:kmhtml": ["type", kmhtml]
+};
+
+// var JASMINE_CORE_PATTERN = /([\\/]karma-jasmine[\\/])/i;
+// var createPattern = function (path) {
+//   return { pattern: path, included: true, served: true, watched: false };
+// };
+
+// var initReporter = function (files, baseReporterDecorator) {
+//   var jasmineCoreIndex = 0;
+
+//   baseReporterDecorator(this);
+
+//   files.forEach(function (file, index) {
+//     if (JASMINE_CORE_PATTERN.test(file.pattern)) {
+//       jasmineCoreIndex = index;
+//     }
+//   });
+
+//   files.splice(++jasmineCoreIndex, 0, createPattern(__dirname + '/css/jasmine.css'));
+//   files.splice(++jasmineCoreIndex, 0, createPattern(__dirname + '/lib/html.jasmine.reporter.js'));
+//   files.splice(++jasmineCoreIndex, 0, createPattern(__dirname + '/lib/adapter.js'));
+// };
+
+// initReporter.$inject = ['config.files', 'baseReporterDecorator'];
+
+// module.exports = {
+//   'reporter:kjhtml': ['type', initReporter]
+// };
